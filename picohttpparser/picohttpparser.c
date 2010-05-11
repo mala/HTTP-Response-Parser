6bc1be73241efa97361fcbab5cd628725c26c777
mala <github@ma.la>
2010-05-11 20:39:32 +0900

    update picohttpparser

--- picohttpparser/picohttpparser.c
+++ picohttpparser/picohttpparser.c
@@ -3,46 +3,53 @@
 
 #define CHECK_EOF()	\
   if (buf == buf_end) { \
-    return -2;		\
+    *ret = -2;		\
+    return NULL;	\
   }
 
 #define EXPECT_CHAR(ch) \
   CHECK_EOF();		\
   if (*buf++ != ch) {	\
-    return -1;		\
+    *ret = -1;		\
+    return NULL;	\
   }
 
-#define ADVANCE_TOKEN()			       \
-  for (; ; ++buf) {			       \
-    CHECK_EOF();			       \
-    if (*buf == ' ') {			       \
-      break;				       \
-    } else if (*buf == '\r' || *buf == '\n') { \
-      return -1;			       \
-    }					       \
-  }
+#define ADVANCE_TOKEN(tok, toklen) do {		       \
+    const char* tok_start = buf; 		       \
+    for (; ; ++buf) {				       \
+      CHECK_EOF();				       \
+      if (*buf == ' ') {			       \
+	break;					       \
+      } else if (*buf == '\r' || *buf == '\n') {       \
+	*ret = -1;				       \
+	return NULL;				       \
+      }						       \
+    }						       \
+    tok = tok_start;				       \
+    toklen = buf - tok_start;			       \
+  } while (0)
 
 #define ADVANCE_EOL(tok, toklen) do {	      \
-    tok = buf;				      \
+    const char* tok_start = buf;	      \
     for (; ; ++buf) {			      \
-      if (buf == buf_end) {                   \
-        toklen = buf - tok;                   \
-        return -2;                            \
-      }			                      \
+      CHECK_EOF();			      \
       if (*buf == '\r' || *buf == '\n') {     \
 	break;				      \
       }					      \
     }					      \
-    toklen = buf - tok;			      \
     if (*buf == '\r') {			      \
       ++buf;				      \
       EXPECT_CHAR('\n');		      \
+      toklen = buf - 2 - tok_start;	      \
     } else { /* should be: *buf == '\n' */    \
+      toklen = buf - tok_start;		      \
       ++buf;				      \
     }					      \
+    tok = tok_start;			      \
   } while (0)
   
-static int is_complete(const char* buf, const char* buf_end, size_t last_len)
+static const char* is_complete(const char* buf, const char* buf_end,
+			       size_t last_len, int* ret)
 {
   int ret_cnt = 0;
   buf = last_len < 3 ? buf : buf + last_len - 3;
@@ -62,58 +69,52 @@ static int is_complete(const char* buf, const char* buf_end, size_t last_len)
       ret_cnt = 0;
     }
     if (ret_cnt == 2) {
-      return 0;
+      return buf;
     }
   }
   
-  return -2;
+  *ret = -2;
+  return NULL;
 }
 
 /* *_buf is always within [buf, buf_end) upon success */
-static int parse_int(const char** _buf, const char* buf_end, int* value)
+static const char* parse_int(const char* buf, const char* buf_end, int* value,
+			     int* ret)
 {
-  const char* buf = *_buf;
-  
   CHECK_EOF();
   if (! ('0' <= *buf && *buf <= '9')) {
-    return -1;
+    *ret = -1;
+    return NULL;
   }
-  *value = 0;
+  int v = 0;
   for (; ; ++buf) {
     CHECK_EOF();
     if ('0' <= *buf && *buf <= '9') {
-      *value = *value * 10 + *buf - '0';
+      v = v * 10 + *buf - '0';
     } else {
       break;
     }
   }
   
-  *_buf = buf;
-  return 0;
+  *value = v;
+  return buf;
 }
 
 /* returned pointer is always within [buf, buf_end), or null */
-static int parse_http_version(const char** _buf, const char* buf_end,
-			      int* minor_version)
+static const char* parse_http_version(const char* buf, const char* buf_end,
+				      int* minor_version, int* ret)
 {
-  const char* buf = *_buf;
-  int r;
-  
   EXPECT_CHAR('H'); EXPECT_CHAR('T'); EXPECT_CHAR('T'); EXPECT_CHAR('P');
   EXPECT_CHAR('/'); EXPECT_CHAR('1'); EXPECT_CHAR('.');
-  
-  if ((r = parse_int(&buf, buf_end, minor_version)) != 0) {
-    return r;
-  }
-  
-  *_buf = buf;
-  return 0;
+  return parse_int(buf, buf_end, minor_version, ret);
 }
 
-static int parse_headers(const char* buf, const char* _buf, const char* buf_end,
-			 struct phr_header* headers, size_t* num_headers, size_t max_headers)
+static const char* parse_headers(const char* buf, const char* buf_end,
+				 struct phr_header* headers,
+				 size_t* num_headers, size_t max_headers,
+				 int* ret)
 {
-  for (*num_headers = 0; ; ++*num_headers) {
+  for (; ; ++*num_headers) {
     CHECK_EOF();
     if (*buf == '\r') {
       ++buf;
@@ -124,7 +125,8 @@ static int parse_headers(const char* buf, const char* _buf, const char* buf_end,
       break;
     }
     if (*num_headers == max_headers) {
-      return -1;
+      *ret = -1;
+      return NULL;
     }
     if (*num_headers == 0 || ! (*buf == ' ' || *buf == '\t')) {
       /* parsing name, but do not discard SP before colon, see
@@ -135,7 +137,8 @@ static int parse_headers(const char* buf, const char* _buf, const char* buf_end,
 	if (*buf == ':') {
 	  break;
 	} else if (*buf < ' ') {
-	  return -1;
+	  *ret = -1;
+	  return NULL;
 	}
       }
       headers[*num_headers].name_len = buf - headers[*num_headers].name;
@@ -152,28 +155,15 @@ static int parse_headers(const char* buf, const char* _buf, const char* buf_end,
     }
     ADVANCE_EOL(headers[*num_headers].value, headers[*num_headers].value_len);
   }
-  return buf - _buf;
+  return buf;
 }
 
-int phr_parse_request(const char* _buf, size_t len, const char** method,
-		      size_t* method_len, const char** path, size_t* path_len,
-		      int* minor_version, struct phr_header* headers,
-		      size_t* num_headers, size_t last_len)
+const char* parse_request(const char* buf, const char* buf_end,
+			  const char** method, size_t* method_len,
+			  const char** path, size_t* path_len,
+			  int* minor_version, struct phr_header* headers,
+			  size_t* num_headers, size_t max_headers, int* ret)
 {
-  const char * buf = _buf, * buf_end = buf + len;
-  int r;
- 
-  size_t max = *num_headers;
-  *num_headers = 0;
- 
-  /* if last_len != 0, check if the request is complete (a fast countermeasure
-     againt slowloris */
-  if (last_len != 0) {
-    if ((r = is_complete(buf, buf_end, last_len)) != 0) {
-      return r;
-    }
-  }
-  
   /* skip first empty line (some clients add CRLF after POST content) */
   CHECK_EOF();
   if (*buf == '\r') {
@@ -184,16 +174,12 @@ int phr_parse_request(const char* _buf, size_t len, const char** method,
   }
   
   /* parse request line */
-  *method = buf;
-  ADVANCE_TOKEN();
-  *method_len = buf - *method;
+  ADVANCE_TOKEN(*method, *method_len);
   ++buf;
-  *path = buf;
-  ADVANCE_TOKEN();
-  *path_len = buf - *path;
+  ADVANCE_TOKEN(*path, *path_len);
   ++buf;
-  if ((r = parse_http_version(&buf, buf_end, minor_version)) != 0) {
-    return r;
+  if ((buf = parse_http_version(buf, buf_end, minor_version, ret)) == NULL) {
+    return NULL;
   }
   if (*buf == '\r') {
     ++buf;
@@ -201,52 +187,104 @@ int phr_parse_request(const char* _buf, size_t len, const char** method,
   } else if (*buf == '\n') {
     ++buf;
   } else {
-    return -1;
+    *ret = -1;
+    return NULL;
   }
   
-  return parse_headers(buf, _buf, buf_end, headers, num_headers, max);
+  return parse_headers(buf, buf_end, headers, num_headers, max_headers, ret);
 }
 
-int phr_parse_response(const char* _buf, size_t len, int* minor_version,
+int phr_parse_request(const char* buf_start, size_t len, const char** method,
+		      size_t* method_len, const char** path, size_t* path_len,
+		      int* minor_version, struct phr_header* headers,
+		      size_t* num_headers, size_t last_len)
+{
+  const char * buf = buf_start, * buf_end = buf_start + len;
+  size_t max_headers = *num_headers;
+  int r;
+  
+  *method = NULL;
+  *method_len = 0;
+  *path = NULL;
+  *path_len = 0;
+  *minor_version = 0;
+  *num_headers = 0;
+  
+  /* if last_len != 0, check if the request is complete (a fast countermeasure
+     againt slowloris */
+  if (last_len != 0 && is_complete(buf, buf_end, last_len, &r) == NULL) {
+    return r;
+  }
+  
+  if ((buf = parse_request(buf, buf_end, method, method_len, path, path_len,
+			   minor_version, headers, num_headers, max_headers,
+			   &r))
+      == NULL) {
+    return r;
+  }
+  
+  return buf - buf_start;
+}
+
+static const char* parse_response(const char* buf, const char* buf_end,
+				  int* minor_version, int* status,
+				  const char** msg, size_t* msg_len,
+				  struct phr_header* headers,
+				  size_t* num_headers, size_t max_headers,
+				  int* ret)
+{
+  /* parse "HTTP/1.x" */
+  if ((buf = parse_http_version(buf, buf_end, minor_version, ret)) == NULL) {
+    return NULL;
+  }
+  /* skip space */
+  if (*buf++ != ' ') {
+    *ret = -1;
+    return NULL;
+  }
+  /* parse status code */
+  if ((buf = parse_int(buf, buf_end, status, ret)) == NULL) {
+    return NULL;
+  }
+  /* skip space */
+  if (*buf++ != ' ') {
+    *ret = -1;
+    return NULL;
+  }
+  /* get message */
+  ADVANCE_EOL(*msg, *msg_len);
+  
+  return parse_headers(buf, buf_end, headers, num_headers, max_headers, ret);
+}
+
+int phr_parse_response(const char* buf_start, size_t len, int* minor_version,
 		       int* status, const char** msg, size_t* msg_len,
 		       struct phr_header* headers, size_t* num_headers,
 		       size_t last_len)
 {
-  const char * buf = _buf, * buf_end = buf + len;
+  const char * buf = buf_start, * buf_end = buf + len;
+  size_t max_headers = *num_headers;
   int r;
- 
-  size_t max = *num_headers; 
+  
+  *minor_version = 0;
+  *status = 0;
+  *msg = NULL;
+  *msg_len = 0;
   *num_headers = 0;
- 
+  
   /* if last_len != 0, check if the response is complete (a fast countermeasure
      against slowloris */
-  if (last_len != 0) {
-    if ((r = is_complete(buf, buf_end, last_len)) != 0) {
-      return r;
-    }
-  }
-  if (buf == buf_end){ return -1; }
-
-  /* parse "HTTP/1.x" */
-  if ((r = parse_http_version(&buf, buf_end, minor_version)) != 0) {
+  if (last_len != 0 && is_complete(buf, buf_end, last_len, &r) == NULL) {
     return r;
   }
-  /* skip space */
-  if (*buf++ != ' ') {
-    return -1;
-  }
-  /* parse status code */
-  if ((r = parse_int(&buf, buf_end, status)) != 0) {
+  
+  if ((buf = parse_response(buf, buf_end, minor_version, status, msg, msg_len,
+			    headers, num_headers, max_headers, &r))
+      == NULL) {
     return r;
   }
-  /* skip space */
-  if (*buf++ != ' ') {
-    return -1;
-  }
-  /* get message */
-  ADVANCE_EOL(*msg, *msg_len);
- 
-  return parse_headers(buf, _buf, buf_end, headers, num_headers, max);
+  
+  return buf - buf_start;
 }
 
 #undef CHECK_EOF
